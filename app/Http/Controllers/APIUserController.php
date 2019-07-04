77acd4d9175a8cd8c524db79fce4c98a2a3d67c6
arnel <arnelchuabonifacio@gmail.com>
2019-07-04 21:56:28 +0800

    Upgrade laravel from 5.4 - 5.8

--- app/Http/Controllers/APIUserController.php
+++ app/Http/Controllers/APIUserController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
-class UserController extends Controller
+class APIUserController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,16 +12,6 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
     {
         //
     }
@@ -45,17 +34,6 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
